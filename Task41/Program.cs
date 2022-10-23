371449b240d7fc098f37f6bebdc7034d409fe221
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-23 22:21:11 +0500

    Add task 41 for sixth seminar

---
+++ Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+System.Console.WriteLine("Enter five numbers:");
+int numberFirst = int.Parse(Console.ReadLine());
+int numberSecond = int.Parse(Console.ReadLine());
+int numberThird = int.Parse(Console.ReadLine());
+int numberFourth = int.Parse(Console.ReadLine());
+int numberFifth = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+if (numberFirst > 0) count++;
+if (numberSecond > 0) count++;
+if (numberThird > 0) count++;
+if (numberFourth > 0) count++;
+if (numberFifth > 0) count++;
+
+System.Console.WriteLine($"Вы ввели {count} чисел(а) больше 0");
